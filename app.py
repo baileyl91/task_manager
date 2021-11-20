044a020a3668546b56e4df89f69d733ec7610604
Luke Bailey <baileyl1991@gmail.com>
2021-11-20 12:31:46 +0000

    Add search functionality

--- app.py
+++ app.py
@@ -185,6 +185,13 @@ def delete_category(category_id):
     return redirect(url_for("get_categories"))
 
 
+@app.route("/search", method=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    tasks = list(mongo.db.tasks.find({"$text": {"$search": query}}))
+    return render_template("tasks.html", tasks=tasks)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
